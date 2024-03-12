6008d46ad1172f3376fe7d4c7dfef7c20bad7e82
Harshsinha2002 <harshsinha2002@gmail.com>
2024-03-12 16:26:04 +0530

    changes

--- app/src/main/java/com/example/test_app/MainActivity.java
+++ app/src/main/java/com/example/test_app/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.test_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    String hello = "Hellloooo";
+    int char_count = 0;
+    CollapsingToolbarLayout collapsingToolbarLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CoordinatorLayout coordinatorLayout = findViewById(R.id.coordinator_layout);
+        AppBarLayout appBarLayout = findViewById(R.id.app_bar_layout);
+        collapsingToolbarLayout = findViewById(R.id.collapsing_toolbar_layout);
+        causedelay(hello);
+    }
+
+    private void causedelay(String hello)
+    {
+        if(char_count <= hello.length())
+        {
+            String fetchedTxt = hello.substring(0,char_count);
+            collapsingToolbarLayout.setTitle(fetchedTxt);
+            new Handler().postDelayed(new Runnable()
+            {
+                @Override
+                public void run()
+                {
+                    char_count++;
+                    causedelay(hello);
+                }
+            }, 200);
+        }
     }
 }
